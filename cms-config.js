cd3ce3c95b54215d53f4027465ebc4b1873822eb
luther casimir <echelonfitnesscollective@gmail.com>
2026-07-23 13:09:31 -0400

    Launch VL Body Lab 2026 site and CMS

---
+++ cms-config.js
@@ -0,0 +1,6 @@
+window.VL_CMS_CONFIG = {
+  url: 'https://plkdyvtriajpzcfgtwzp.supabase.co',
+  key: 'sb_publishable_CwFNrWSrhLKURZIk_-yt1A_ZVpFHEwf',
+  brand: 'vl-body-lab',
+  table: 'brand_content_items'
+};
